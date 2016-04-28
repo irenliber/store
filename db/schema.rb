334509b417f9a36139f5910c97f3328a3625d429
Iren Liber <irr.morgo@gmail.com>
2016-04-28 19:47:04 +0600

    created model "Pages"

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160428091347) do
+ActiveRecord::Schema.define(version: 20160428125608) do
 
   create_table "deliveries", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  create_table "pages", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
     t.datetime "created_at",  null: false
